d288046e71516af9779eaa5da3b4e4ff3c1442d0
KokoKostov <127635232+KokoKostov@users.noreply.github.com>
2023-03-24 02:00:04 +0200

    added Footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import styles from './styles/layout.css'
 import {Service} from './components/Service'
 import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 function App() {
   return (
     <>
+    <div id="homepage">
       <Header/>   
       <Service/>
+      
+      </div>
+      <Footer/>
     </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+export const Footer =()=>{
+return (
+    <div className="wrapper row3">
+  <div id="footer" className="clear"></div>
+  </div>
+)
+}
