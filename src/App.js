73da7bd1525d45aca536a8419d6bfeff802f23b1
KokoKostov <127635232+KokoKostov@users.noreply.github.com>
2023-03-24 02:07:36 +0200

    Added Content component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import styles from './styles/layout.css'
 import {Service} from './components/Service'
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
+import { Content } from './components/Content';
 function App() {
   return (
     <>
     <div id="homepage">
-      <Header/>   
+      <Header/>  
+      <Content/> 
       <Service/>
       
       </div>
---
+++ src/components/Content.js
@@ -0,0 +1,24 @@
+export const Content = ()=>{
+    return (
+        <div ClassName="wrapper row2">
+  <div id="container" ClassName="clear">
+ 
+    <section id="slider"><a href="/"><img src="images/demo/960x360.gif" alt=""/></a></section>
+    
+      <section id="intro" ClassName="last clear">
+        <article>
+          <figure><img src="images/demo/450x250.gif" width="450" height="250" alt=""/>
+            <figcaption>
+              <h2>Top Rated Drawing</h2>
+              <p>This is a W3C compliant free website template from <a href="https://www.os-templates.com/" title="Free Website Templates">OS Templates</a>. For full terms of use of this template please read our <a href="https://www.os-templates.com/template-terms">website template licence</a>.</p>
+              <p>You can use and modify the template for both personal and commercial use. You must keep all copyright information and credit links in the template and associated files. For more HTML5 templates visit <a href="https://www.os-templates.com/">free website templates</a>.</p>
+              <footer ClassName="more"><a href="/">Read More &raquo;</a></footer>
+            </figcaption>
+          </figure>
+        </article>
+      </section>
+      
+    </div>
+    </div>
+    )
+}
